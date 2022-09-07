cbc3ad75191e0b8c9edb7bc679619940d7a556e3
noah <42802689+AmashiroNoah@users.noreply.github.com>
2022-09-07 11:40:32 +0800

    Image Test

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ module.exports = {
   i18n,
   reactStrictMode: true,
   // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
-  trailingSlash: true
-}
-module.exports = {
+  trailingSlash: true,
   images: {
     domains: ['share.kivotos.top','noah-onedrive-vercel-index.vercel.app'],
   },
